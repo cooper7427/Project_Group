763151f3c327f3e1794271c5f3e6f621e1faf958
cooper7427 <cooper7427@gmail.com>
2019-11-15 20:13:33 +0500

    Размещение элементов на форму 3

--- project/project/Form3.Designer.cs
+++ project/project/Form3.Designer.cs
@@ -28,19 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvCustomerOrders = new System.Windows.Forms.DataGridView();
+            this.btnFindByOrderID = new System.Windows.Forms.Button();
+            this.btnCancelOrder = new System.Windows.Forms.Button();
+            this.btnFillOrder = new System.Windows.Forms.Button();
+            this.btnFinishUpdates = new System.Windows.Forms.Button();
+            this.dtpFillDate = new System.Windows.Forms.DateTimePicker();
+            this.txtOrderID = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomerOrders)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Идентификатор заказа:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "При заполнении заказа укажите дату заполнения:";
+            // 
+            // dgvCustomerOrders
+            // 
+            this.dgvCustomerOrders.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCustomerOrders.Location = new System.Drawing.Point(26, 97);
+            this.dgvCustomerOrders.Name = "dgvCustomerOrders";
+            this.dgvCustomerOrders.ReadOnly = true;
+            this.dgvCustomerOrders.RowHeadersVisible = false;
+            this.dgvCustomerOrders.Size = new System.Drawing.Size(468, 110);
+            this.dgvCustomerOrders.TabIndex = 2;
+            // 
+            // btnFindByOrderID
+            // 
+            this.btnFindByOrderID.Location = new System.Drawing.Point(271, 21);
+            this.btnFindByOrderID.Name = "btnFindByOrderID";
+            this.btnFindByOrderID.Size = new System.Drawing.Size(115, 23);
+            this.btnFindByOrderID.TabIndex = 3;
+            this.btnFindByOrderID.Text = "Найти заказ";
+            this.btnFindByOrderID.UseVisualStyleBackColor = true;
+            this.btnFindByOrderID.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCancelOrder
+            // 
+            this.btnCancelOrder.Location = new System.Drawing.Point(26, 225);
+            this.btnCancelOrder.Name = "btnCancelOrder";
+            this.btnCancelOrder.Size = new System.Drawing.Size(111, 23);
+            this.btnCancelOrder.TabIndex = 4;
+            this.btnCancelOrder.Text = "Отменить заказ";
+            this.btnCancelOrder.UseVisualStyleBackColor = true;
+            // 
+            // btnFillOrder
+            // 
+            this.btnFillOrder.Location = new System.Drawing.Point(158, 225);
+            this.btnFillOrder.Name = "btnFillOrder";
+            this.btnFillOrder.Size = new System.Drawing.Size(106, 23);
+            this.btnFillOrder.TabIndex = 5;
+            this.btnFillOrder.Text = "Заполнить заказ";
+            this.btnFillOrder.UseVisualStyleBackColor = true;
+            // 
+            // btnFinishUpdates
+            // 
+            this.btnFinishUpdates.Location = new System.Drawing.Point(419, 225);
+            this.btnFinishUpdates.Name = "btnFinishUpdates";
+            this.btnFinishUpdates.Size = new System.Drawing.Size(75, 23);
+            this.btnFinishUpdates.TabIndex = 6;
+            this.btnFinishUpdates.Text = "Готово";
+            this.btnFinishUpdates.UseVisualStyleBackColor = true;
+            // 
+            // dtpFillDate
+            // 
+            this.dtpFillDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFillDate.Location = new System.Drawing.Point(294, 61);
+            this.dtpFillDate.Name = "dtpFillDate";
+            this.dtpFillDate.Size = new System.Drawing.Size(200, 20);
+            this.dtpFillDate.TabIndex = 7;
+            // 
+            // txtOrderID
+            // 
+            this.txtOrderID.Location = new System.Drawing.Point(158, 23);
+            this.txtOrderID.Name = "txtOrderID";
+            this.txtOrderID.Size = new System.Drawing.Size(94, 20);
+            this.txtOrderID.TabIndex = 8;
             // 
             // FillOrCancel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(585, 329);
+            this.ClientSize = new System.Drawing.Size(522, 263);
+            this.Controls.Add(this.txtOrderID);
+            this.Controls.Add(this.dtpFillDate);
+            this.Controls.Add(this.btnFinishUpdates);
+            this.Controls.Add(this.btnFillOrder);
+            this.Controls.Add(this.btnCancelOrder);
+            this.Controls.Add(this.btnFindByOrderID);
+            this.Controls.Add(this.dgvCustomerOrders);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "FillOrCancel";
-            this.Text = "FillOrCancel";
+            this.Text = "Заполнение или отмена заказов";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCustomerOrders)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvCustomerOrders;
+        private System.Windows.Forms.Button btnFindByOrderID;
+        private System.Windows.Forms.Button btnCancelOrder;
+        private System.Windows.Forms.Button btnFillOrder;
+        private System.Windows.Forms.Button btnFinishUpdates;
+        private System.Windows.Forms.DateTimePicker dtpFillDate;
+        private System.Windows.Forms.TextBox txtOrderID;
     }
 }
